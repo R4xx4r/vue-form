c89fbe776a63488113a75f3946be756a17be7caf
Daniel M <daniel.murth@chello.at>
2019-05-27 21:28:46 +0200

    [TASK] start implementing components

--- src/components/Example.vue
+++ src/components/Example.vue
@@ -1,12 +1,28 @@
 <template>
-  <form>
-    EXAMPLE FORM
+
+  <form class="vue-form__form" :id="formData.id">
+
+    <FormRow v-for="(row, index) in formData.rows" :key="index">
+
+      <template v-for="obj in row">
+        
+
+      </template>
+
+    </FormRow>
+
   </form>
+
 </template>
 
 <script>
+  import FormRow from '@/components/partials/FormRow.vue';
+
   export default {
     name: 'Form',
+    components: {
+      FormRow
+    },
     props: {
       formData: {
         type: Object,
@@ -19,3 +35,11 @@
     }
   }
 </script>
+
+<style>
+ .vue-form__form {
+   display: flex;
+   flex-wrap: wrap;
+   width: 100%;
+ }
+</style>
---
+++ src/components/partials/FormRow.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="vue-form__form-row">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "FormRow"
+  }
+</script>
+
+<style>
+  .vue-form__form-row {
+    display: flex;
+    width: 100%;
+  }
+</style>
